8d9640e64af195f1c67d4a636eff1d30ed80cbfd
Stephanie Unna <steph.unna@gmail.com>
2019-08-28 08:29:15 +0100

    added thin border to flag, lower case country with no spaces

---
+++ src/Flag.svelte
@@ -0,0 +1,20 @@
+<script>
+    let country = 'uganda';
+</script>
+
+<style>
+    div {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+    img, input {
+        border: 2px solid #efede6;
+        width: 200px;
+    }
+</style>
+
+<div>
+    <input bind:value={country} />
+    <img src='https://raw.githubusercontent.com/Treblesteph/svelte-playing/master/src/flags/{country.toLowerCase().split(" ").join("")}.png' alt='flag of {country}'/>
+</div>
