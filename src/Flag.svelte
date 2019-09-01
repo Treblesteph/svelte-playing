b0744c3f99e7d0040989bcaf1f95590179be0e13
Stephanie Unna <steph.unna@gmail.com>
2019-09-01 10:45:26 +0100

    remove unused styles

--- src/Flag.svelte
+++ src/Flag.svelte
@@ -32,62 +32,6 @@
         align-items: center;
         flex-direction: column;
     }
-    img, input.answer {
-        border: 2px solid #efede6;
-        width: 200px;
-        margin: 2em;
-    }
-    .correct {
-        color: green;
-    }
-    .incorrect {
-        color: red;
-    }
-    .switch {
-        position: absolute;
-        right: 9em;
-        bottom: 10em;
-        display: inline-block;
-        width: 40px;
-        height: 20px;
-    }
-    .switch input[type="checkbox"] {
-        display: none;
-    }
-    .switch input:checked + .knob {
-        animation: colorChange 0.4s linear forwards;
-    }
-    .switch input:checked + .knob:before {
-        animation: turnON 0.4s linear forwards;
-    }
-    @keyframes colorChange {
-        from { background-color: #ccc; }
-        50% { background-color: #A4D9AD; }
-        to { background-color: #4BD663; }
-    }
-    @keyframes turnON {
-        from { transform: translateX(0px); }
-        to { transform: translateX(20.2px); }
-    }
-    .knob {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #ccc;
-        border-radius: 20px;
-    }
-    .knob:before {
-        position: absolute;
-        background-color: white;
-        content: "";
-        left: 2px;
-        top: 2px;
-        width: 16px;
-        height: 16px;
-        border-radius: 50%;
-    }
 </style>
 
 <div>
