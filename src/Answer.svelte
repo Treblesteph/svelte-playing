13ee8e2972ace5cf20936068d63a1b1079a11e52
Stephanie Unna <steph.unna@gmail.com>
2019-09-01 10:42:49 +0100

    break out answer code

---
+++ src/Answer.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { stripString } from './Utils.js';
+
+    export let correctanswer;
+    export let guessanswer;
+    export let handleClick;
+
+    $: correct = stripString(guessanswer) === stripString(correctanswer);
+</script>
+
+<style>
+    .correct {
+        color: green;
+    }
+    .incorrect {
+        color: red;
+    }
+</style>
+
+<p>You have guessed {guessanswer}...</p>
+{#if correct}
+    <p class='correct'>✅ that is correct!</p>
+    <button value='next' on:click={handleClick}>Next Flag</button>
+{:else}
+    <p class='incorrect'>❌ that is incorrect try again!</p>
+{/if}
