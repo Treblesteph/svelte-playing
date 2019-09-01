facc1a118f83f66ca67a6884c2a7483254670903
Stephanie Unna <steph.unna@gmail.com>
2019-09-01 10:43:08 +0100

    break out percent correct code

---
+++ src/Percent.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let percent;
+</script>
+
+<style>
+    .percent1 {
+        width: 200px;
+        font-size: 100px;
+        margin: 0;
+        color: cadetblue;
+    }
+    .percent2 {
+        width: 200px;
+        font-size: 68px;
+        margin-top: -35px;
+        color: cadetblue;
+    }
+</style>
+
+<p class='percent1'>{percent}%</p>
+<p class='percent2'>correct</p>
