f778b94c5f4fbb396cb96d6f977f0042d75a403e
Stephanie Unna <steph.unna@gmail.com>
2019-09-08 12:38:49 +0100

    changing name of flag component to game

---
+++ src/Game.svelte
@@ -0,0 +1,61 @@
+<script>
+    import { Countries } from './Countries.js';
+    import { getUnique, shuffle } from './Utils.js';
+    import Answer from './Answer.svelte';
+    import Percent from './Percent.svelte';
+    import Question from './Question.svelte';
+    import Toggle from './Toggle.svelte';
+
+    let difficult;
+    let correctcount = 5;
+    let answeredcount = 10;
+    let guess;
+    let answer;
+    let code;
+    let options;
+
+    $: percent = answeredcount === 0 ? 0 : Math.round(100*correctcount/answeredcount)
+
+    const nextFlag = () => {
+        const countries = Countries.map(c => Object.values(c)[0]);
+        const indexOfAnswer = Math.floor(Math.random()*countries.length);
+        answer = countries[indexOfAnswer];
+        code = Object.keys(Countries[indexOfAnswer])[0];
+        let answercorrect;
+        countries.splice(indexOfAnswer, 1)
+        options = [answer, ...getUnique(countries, 3)]
+        shuffle(options)
+        guess = '';
+    }
+    nextFlag()
+</script>
+
+<style>
+    div {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+</style>
+
+<div>
+    <Question
+        gameType='map'
+        code={code}
+        answer={answer}
+        difficult={difficult}
+        options={options}
+        bind:value={guess}
+    />
+    
+    {#if guess}
+        <Answer
+            correctanswer={answer}
+            guessanswer={guess}
+            handleClick={nextFlag}
+        />
+    {:else}
+        <Toggle bind:difficulty={difficult}/>
+    {/if}
+    <Percent percent={percent} />
+</div>
